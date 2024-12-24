93e7740a58544806fb3fa419a23455ca24723a03
A Meaningful star <ghrlfehd123@naver.com>
2024-12-24 16:41:47 +0900

    edit: 프로젝트 개발에 필요한 디자인 요소 사용자화로 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,23 @@ export default {
   ],
   theme: {
     extend: {
+      fontSize: {
+        "layout-24-b": [
+          "12px",
+          { lineHeight: "100%", letterSpacing: "0%", fontWeight: "400" },
+        ],
+        "layout-28-b": [
+          "28px",
+          { lineHeight: "100%", letterSpacing: "0%", fontWeight: "700" },
+        ],
+      },
       colors: {
+        gl:{
+          "grayscale-100": "rgba(128, 128, 128, 1)",
+          "green-base": "rgba(13, 152, 0, 1)",
+          "green-opacity-30": "rgba(13, 152, 0, 0.3)",
+          "green-opacity-50": "rgba(13, 152, 0, 0.5)",
+        },
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
