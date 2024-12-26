8e1b26ace41d815fecb97d071d874c1950b2650c
A Meaningful star <ghrlfehd123@naver.com>
2024-12-26 16:12:49 +0900

    edit: 프로젝트 스타일 사용자 설정 수정 및 적용

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,11 +6,11 @@ import Logo_Icon from '@icon/logo_icon.png'
 export function Header() {
   return (
     <div className="rounded-2xl bg-gl-green-opacity-50 p-2">
-      <Link href={'/main'} className="flex w-fit items-center gap-x-2.5 rounded-lg hover:bg-gl-green-opacity-30">
+      <Link href={'/'} className="flex w-fit items-center gap-x-2.5 rounded-lg hover:bg-gl-green-opacity-30">
         <Image alt="logo_image" src={Logo_Icon} width={48} height={48} priority={false} quality={75} />
         <div className="flex items-end gap-x-3 pr-2">
           <span className="text-layout-28-b">청신호</span>
-          <span className="text-layout-24-b text-gl-grayscale-100">Administration</span>
+          <span className="text-layout-12-r text-gl-grayscale-300">Administration</span>
         </div>
       </Link>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,34 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       fontSize: {
-        "layout-24-b": [
-          "12px",
-          { lineHeight: "100%", letterSpacing: "0%", fontWeight: "400" },
-        ],
-        "layout-28-b": [
-          "28px",
-          { lineHeight: "100%", letterSpacing: "0%", fontWeight: "700" },
-        ],
+        'layout-12-r': ['12px', { lineHeight: '100%', letterSpacing: '0%', fontWeight: '400' }],
+        'layout-14-r': ['14px', { lineHeight: '100%', letterSpacing: '0%', fontWeight: '400' }],
+        'layout-16-b': ['16px', { lineHeight: '100%', letterSpacing: '0%', fontWeight: '700' }],
+        'layout-28-b': ['28px', { lineHeight: '100%', letterSpacing: '0%', fontWeight: '700' }],
       },
       colors: {
-        gl:{
-          "grayscale-100": "rgba(128, 128, 128, 1)",
-          "green-base": "rgba(13, 152, 0, 1)",
-          "green-opacity-30": "rgba(13, 152, 0, 0.3)",
-          "green-opacity-50": "rgba(13, 152, 0, 0.5)",
+        gl: {
+          'grayscale-100': 'rgba(245, 245, 245, 1)',
+          'grayscale-200': 'rgba(158, 158, 158, 1)',
+          'grayscale-300': 'rgba(128, 128, 128, 1)',
+          'green-base': 'rgba(13, 152, 0, 1)',
+          'green-opacity-30': 'rgba(13, 152, 0, 0.3)',
+          'green-opacity-40': 'rgba(182, 224, 178, 0.4)',
+          'green-opacity-50': 'rgba(13, 152, 0, 0.5)',
         },
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
