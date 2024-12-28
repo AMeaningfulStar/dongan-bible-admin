cd7f0c0fddaf137ae6bfec7819168ee425fabf2d
A Meaningful star <ghrlfehd123@naver.com>
2024-12-28 13:44:33 +0900

    edit: 스타일 사용자 정의 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,12 +13,16 @@ export default {
         'layout-14-r': ['14px', { lineHeight: '100%', letterSpacing: '0%', fontWeight: '400' }],
         'layout-16-b': ['16px', { lineHeight: '100%', letterSpacing: '0%', fontWeight: '700' }],
         'layout-28-b': ['28px', { lineHeight: '100%', letterSpacing: '0%', fontWeight: '700' }],
+        'button-14-l': ['14px', { lineHeight: '100%', letterSpacing: '0%', fontWeight: '400' }],
+        'button-16-m': ['16px', { lineHeight: '100%', letterSpacing: '0%', fontWeight: '500' }],
+        'modal-18-b': ['18px', { lineHeight: '100%', letterSpacing: '0%', fontWeight: '700' }],
       },
       colors: {
         gl: {
           'grayscale-100': 'rgba(245, 245, 245, 1)',
           'grayscale-200': 'rgba(158, 158, 158, 1)',
           'grayscale-300': 'rgba(128, 128, 128, 1)',
+          'grayscale-opacity-50': 'rgba(158, 158, 158, 0.5)',
           'green-base': 'rgba(13, 152, 0, 1)',
           'green-opacity-30': 'rgba(13, 152, 0, 0.3)',
           'green-opacity-40': 'rgba(182, 224, 178, 0.4)',
