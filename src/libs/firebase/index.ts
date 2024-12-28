296538b0bd75e8218b0e9d8b07b05443bd42858b
A Meaningful star <ghrlfehd123@naver.com>
2024-12-28 13:42:51 +0900

    edit: firebase 수정

--- src/libs/firebase/index.ts
+++
@@ -1,4 +0,0 @@
-import { adminApp, adminAuth, adminFirestore } from './admin.firebase'
-import { mainApp, mainAuth, mainDatabase, mainFirestore } from './main.firebase'
-
-export { adminApp, adminAuth, adminFirestore, mainApp, mainAuth, mainDatabase, mainFirestore }
