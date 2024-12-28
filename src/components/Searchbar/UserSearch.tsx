e44473fde12c69c8c840ccbf52b01afa9c6a5dfc
A Meaningful star <ghrlfehd123@naver.com>
2024-12-28 13:51:31 +0900

    edit: Searchbar components - UserSearch
    사용자 등록 및 검색을 위한 컴포넌트

---
+++ src/components/Searchbar/UserSearch.tsx
@@ -0,0 +1,99 @@
+'use client'
+
+import { useRef, useState } from 'react'
+
+import Modal from '@components/Modal'
+
+import { createUsers, readExcel } from '@/utils'
+
+type CreateUserType = {
+  id: string
+  password: string
+  name: string
+  phone: string
+  position: string
+  grade: number
+  class: number
+}
+
+export function UserSearch() {
+  const [isModalShow, setIsModalShow] = useState<boolean>(false)
+  const [file, setFile] = useState<File | null>(null)
+  const [userData, setUserData] = useState<CreateUserType[]>([])
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const handleClick = () => {
+    inputRef.current?.click()
+  }
+
+  const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    try {
+      const files = event.target.files
+
+      if (files) {
+        setFile(files[0])
+        const excelData = await readExcel(files[0])
+        
+        setUserData(excelData)
+      }
+    } catch (error) {
+      console.error('엑셀 파일 처리 오류:', error)
+    }
+  }
+
+  return (
+    <div className="flex w-full gap-x-4 rounded-lg bg-white p-4">
+      <input
+        className="flex-grow rounded border border-gl-grayscale-100 py-3 pl-2 outline-none"
+        placeholder="'사용자 이름'으로 검색하기"
+      />
+      <button className="bg-gl-grayscale-200 text-button-18-m hover:bg-gl-grayscale-300 rounded px-4 py-2 text-white">
+        검색
+      </button>
+      <button
+        className="text-button-18-m rounded bg-gl-green-opacity-50 px-4 py-2 text-white hover:bg-gl-green-base"
+        onClick={() => setIsModalShow(true)}
+      >
+        등록
+      </button>
+      {isModalShow && (
+        <Modal title="사용자 등록" setIsModalShow={setIsModalShow}>
+          <div className="flex w-full flex-col gap-y-4">
+            {/* 개별등록 */}
+            <div></div>
+            <div className="h-px w-full bg-gl-grayscale-100" />
+            {/* 파일등록 */}
+            <div className="flex gap-x-2 px-4">
+              <div className="flex-grow rounded-md border border-gl-grayscale-100 p-4">
+                {file ? (
+                  <span className="">{file.name}</span>
+                ) : (
+                  <span className="text-gl-grayscale-200">파일을 업로드해주세요</span>
+                )}
+              </div>
+              <button
+                className="bg-gl-grayscale-200 text-button-18-m hover:bg-gl-grayscale-300 rounded px-4 py-2 text-white"
+                onClick={handleClick}
+              >
+                파일 선택
+              </button>
+              <button
+                className="text-button-18-m rounded bg-gl-green-opacity-50 px-4 py-2 text-white hover:bg-gl-green-base"
+                onClick={async () => await createUsers(userData)}
+              >
+                등록
+              </button>
+              <input
+                type="file"
+                className="hidden"
+                ref={inputRef}
+                accept=".xlsx, .xls, .csv"
+                onChange={handleFileChange}
+              />
+            </div>
+          </div>
+        </Modal>
+      )}
+    </div>
+  )
+}
