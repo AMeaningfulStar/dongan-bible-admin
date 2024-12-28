6cbd4a68959a3c56634ada7db5fcf2934d0e0df3
A Meaningful star <ghrlfehd123@naver.com>
2024-12-28 15:27:38 +0900

    edit: 초기 랜더링 또는 리랜더링 시 검색 데이터 초기화

--- src/app/users/manage/page.tsx
+++ src/app/users/manage/page.tsx
@@ -3,10 +3,17 @@
 import { Users } from '@/components/List'
 import { UserSearch } from '@/components/Searchbar'
 
+import useSearchData from '@/libs/stores/SearchData'
 import { getUserList } from '@/libs/swr'
+import { useEffect } from 'react'
 
 export default function Users_Manage() {
   const { userList, isLoading, isError } = getUserList()
+  const { searchData, initSearchData } = useSearchData()
+
+  useEffect(() => {
+    initSearchData()
+  }, [])
 
   if (isLoading) {
     return <div>...Loading</div>
@@ -19,7 +26,7 @@ export default function Users_Manage() {
   return (
     <div className="flex h-full flex-col gap-y-2">
       <UserSearch />
-      <Users userList={userList?.data || []} />
+      <Users userList={searchData ? searchData : userList?.data || []} />
     </div>
   )
 }
